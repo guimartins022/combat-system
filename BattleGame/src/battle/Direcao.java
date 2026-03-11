8c93dcb0b163e38a9e829a05f90160da8f5f6b31
guimartins022 <guimartinsoliveira022@gmail.com>
2026-03-10 21:42:46 -0300

    Batalha de um herói contra o monstro.

---
+++ BattleGame/src/battle/Direcao.java
@@ -0,0 +1,7 @@
+package battle;
+
+public enum Direcao {
+
+	NORTE, LESTE, SUL, OESTE;
+	
+}
---
+++ BattleGame/src/battle/Jogador.java
@@ -0,0 +1,51 @@
+package battle;
+
+public class Jogador { // Super classe.
+
+	int vida = 100;
+	int x;
+	int y;
+	
+	Jogador(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	// Os jogadores só se atacam se estiverem um do lado do outro:
+	boolean atacar(Jogador oponente) {
+		int deltaX = Math.abs(x - oponente.x);
+		int deltaY = Math.abs(y - oponente.y);
+		
+		if(deltaX == 0 && deltaY == 1) {
+			oponente.vida -= 10;
+			return true;
+		} else if(deltaX == 1 && deltaY == 0) {
+			oponente.vida -= 10;			
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	// Movimentação:
+	boolean andar(Direcao direcao) {
+		switch(direcao) {
+		case NORTE:
+			y--;
+			break;
+		case LESTE:
+			x++;
+			break;
+		case SUL:
+			y++;
+			break;
+		case OESTE:
+			x--;
+			break;
+		}
+		
+		return true;
+	}
+	
+}
